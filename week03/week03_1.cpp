5f9a4ce414149d8051246d00d799a88f46ef9cbd
yuni0719 <nicolewu0719@gmail.com>
2024-03-06 09:59:38 +0800

    week03

---
+++ week03/week03_1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("請輸入2個數字:");
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%5d\n", a);
+    printf("%5d\n", b);
+    printf("---------\n");
+    printf("%5d\n", a+b);
+}
