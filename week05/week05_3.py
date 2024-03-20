c23298544d20e265cbf725b10a8cf52011cc7fad
yuni0719 <nicolewu0719@gmail.com>
2024-03-20 11:24:27 +0800

    week05

---
+++ week05/week05_3.py
@@ -0,0 +1,8 @@
+for i in range(9): #0...8 (小於9),共9個
+  print('i是', i, end=' ')
+print() #跳行
+for i in range(1, 9+1): #1...9(左右都加1格),共9個
+  print('i是', i, end=' ')
+print() #跳行
+for i in range(9-1, -1, -1):
+  print('i是', i, end=' ')
