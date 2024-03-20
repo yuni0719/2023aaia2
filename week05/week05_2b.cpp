3c1881d3de484c09f459a0ccc948b581d00a451c
yuni0719 <nicolewu0719@gmail.com>
2024-03-20 10:50:56 +0800

    week05

--- week05/week05_2b.cpp
+++ week05/week05_2b.cpp
@@ -1,9 +1,10 @@
-#右鍵按Format use Astyle
+///Settings-Editor (左邊第七個)Source Formatter
+///選KAR(C的爸爸)
+///右鍵按Format use Astyle
 #include <stdio.h>
 int main()
 {
-    for (int i=0; i<3; i++)
-    {
+    for (int i=0; i<3; i++) {
         printf("i是%d", i);
     }
 }
